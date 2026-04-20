290fae695fe12b657de0f6c7cbe293b3aa47d3e6
Oswaldo <santoswaldos@gmail.com>
2026-04-20 14:11:29 -0600

    Método 6, agendaLlena verifica cada índice para verificar si tiene contenido

--- src/Agenda/Agenda.java
+++ src/Agenda/Agenda.java
@@ -42,9 +42,16 @@ public class Agenda {
 
     }
 
+    //Verificar si la agenda esta llena
     public Boolean agendaLlena(){
-
-
+        //Se verifica cada índice del arreglo, en caso que algun índice sea NULL entonces aún tiene espacio
+        for(int i = 0; i<contacto.length; i++){
+            if(contacto[i] == null){
+                return false;
+            }
+        }
+        //Sino todo es diferente de NULL entonces esta llena
+        return true;
     }
     //Aaron
 
