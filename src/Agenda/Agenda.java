3f9f4b4e72a1783b84695cb81cc3b44137c55d9c
Oswaldo <santoswaldos@gmail.com>
2026-04-20 14:25:00 -0600

    Método 5, eliminarContacto al verificar si existe el contacto

--- src/Agenda/Agenda.java
+++ src/Agenda/Agenda.java
@@ -4,49 +4,40 @@ public class Agenda {
     //Aaron
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-     //Jesus
-
-
-
-
-
-
-
+    //Jesus
 
 
     //Vero
 
 
-
-
-
-
-
-
-
     //Oswaldo
 
-    public void eliminarContacto (Contacto c){
+    public void eliminarContacto(Contacto c) {
+        if (c == null) {
+            System.out.println("No se puede eliminar un contacto nulo...");
 
+        }
+
+        boolean estado = false;
+        for (int i = 0; i < contactos.length; i++) {
+            if (contactos[i] != null && contactos[i].equals[c]) {
+                estado = true;
+                contactos[i] = null;
+                System.out.println("Contacto eliminado correctamente...");
+                break;
+            }
+        }
+
+        if(!estado){
+            System.out.println("No se encontro el contacto");
+        }
     }
 
     //Verificar si la agenda esta llena
-    public Boolean agendaLlena(){
+    public Boolean agendaLlena() {
         //Se verifica cada índice del arreglo, en caso que algun índice sea NULL entonces aún tiene espacio
-        for(int i = 0; i<contacto.length; i++){
-            if(contacto[i] == null){
+        for (int i = 0; i < contacto.length; i++) {
+            if (contacto[i] == null) {
                 return false;
             }
         }
@@ -54,14 +45,6 @@ public class Agenda {
         return true;
     }
     //Aaron
-
-
-
-
-
-
-
-
 
 
 }
